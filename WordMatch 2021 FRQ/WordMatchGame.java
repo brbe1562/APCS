adc64796d989f25fd8db70842d06aa2a995b4314
brbe1562 <bradleyaran@gmail.com>
2024-02-29 23:06:23 -0800

    Update WordMatchGame.java

--- WordMatch 2021 FRQ/WordMatchGame.java
+++ WordMatch 2021 FRQ/WordMatchGame.java
@@ -1,21 +1,21 @@
-import java.util.*; import java.io.*; 
-public class WordMatchGame {     
+import java.util.*;
+public class WordMatchGame{     
     private String secret;     
-    public WordMatchGame(String word) {         
+    public WordMatchGame(String word){         
         this.secret = word.toLowerCase();     
     }     
-    public int scoreGuess(String guess)     {           
+    public int scoreGuess(String guess){           
         String s = secret;         
         int occur = 0;         
-        while( s.length() > 0 && s.indexOf(guess) != -1 )         {
+        while( s.length() > 0 && s.indexOf(guess) != -1 ){
             int k = s.indexOf(guess);
             s = s.substring(k + 1);
             occur++;         
         }
         return occur * guess.length() * guess.length();
     }     
-    public String findBetterGuess(String guess1, String guess2)     {
-        if (scoreGuess(guess1) == scoreGuess(guess2))         {
+    public String findBetterGuess(String guess1, String guess2){
+        if (scoreGuess(guess1) == scoreGuess(guess2)){
             if (guess1.compareTo(guess2) > 0)                 
                 return guess1;             
             return guess2;         
@@ -24,10 +24,10 @@ public class WordMatchGame {
             return guess1;         
         return guess2;         
     }     
-    public String getSecret() {
+    public String getSecret(){
         return secret;
     }
-    public static void main(String[] args)     {
+    public static void main(String[] args){
         Scanner k = new Scanner(System.in);
         System.out.print("Enter Secret Word: ");
         String secret = k.nextLine();         
@@ -39,12 +39,13 @@ public class WordMatchGame {
         int maxPoints = game.scoreGuess(secret);         
         int points = 0;         
         System.out.println("Take a guess: ");         
-        while (points != maxPoints)         {
+        while (points != maxPoints){
             g = k.nextLine();             
             points = game.scoreGuess(g);             
             System.out.println(points);             
             if (points != maxPoints)                 
-                System.out.println("Take another guess: ");         }         
+                System.out.println("Take another guess: ");
+        }         
         System.out.print("Congrats you win!!!!!");     
     }
 }
